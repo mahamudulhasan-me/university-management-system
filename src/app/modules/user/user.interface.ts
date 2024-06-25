3f8188cb64acda6f3ee0584bf907fd04a46ab4d9
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-06-25 21:35:20 +0600

    up date nsql

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -7,6 +7,7 @@ export interface IUser {
   role: "admin" | "student" | "faculty";
   status: "active" | "inactive";
   isDeleted: boolean;
+  passwordChangeAt?: Date;
 }
 
 // export interface INewUser {
